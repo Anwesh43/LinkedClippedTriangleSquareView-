d9b085e4f3394c7b22d7a9bbae84cbcf588d61dc
Anwesh43 <anweshthecool0@gmail.com>
2020-10-24 01:11:43 +0530

    adding ClippedTriangleSquare class

--- clippedtrianglesquareview/src/main/java/com/anwesh/uiprojects/clippedtrianglesquareview/ClippedTriangleSquareView.kt
+++ clippedtrianglesquareview/src/main/java/com/anwesh/uiprojects/clippedtrianglesquareview/ClippedTriangleSquareView.kt
@@ -188,4 +188,27 @@ class ClippedTriangleSquareView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ClippedTriangleSquare(var i : Int) {
+
+        private var curr : CTSNode = CTSNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
